3754764977d56e0c37c87d6cd0c5526aeefeb7f1
Derek Slater <60079702+Derek-Slater@users.noreply.github.com>
2022-03-15 23:03:22 -0700

    Added some blank meme templates

--- manage_memes.py
+++ manage_memes.py
@@ -1,12 +1,11 @@
 
 mock_data = [
-  "https://cdn.discordapp.com/attachments/950813918350180402/950857787007729724/unknown.png",
-  "https://cdn.discordapp.com/attachments/603796374982754304/948050557946454126/unknown.png",
-  "https://cdn.discordapp.com/attachments/603796374982754304/947032437274320906/unknown.png",
-  "https://cdn.discordapp.com/attachments/603796374982754304/943924296772911144/unknown.png",
-  "https://cdn.discordapp.com/attachments/795549507588063252/943540742163415060/IMG_8588.jpg",
-  "https://preview.redd.it/swsqeffnidm81.jpg?width=640&crop=smart&auto=webp&s=5aa611521c339731fe1e6d1d808f5f8f15fa13c4",
-  "https://i.redd.it/6s96i19r8bm81.jpg"
+  "https://cdn.discordapp.com/attachments/828079037565108265/953530540135424071/unknown.png",
+  "https://cdn.discordapp.com/attachments/940733112692932668/950201782297034752/Screen_Shot_2020-10-05_at_11.png",
+  "https://cdn.discordapp.com/attachments/828079037565108265/953530905387999232/unknown.png",
+  "https://media.discordapp.net/attachments/828079037565108265/953531153858576394/unknown.png",
+  "https://cdn.discordapp.com/attachments/828079037565108265/953531433341845594/unknown.png",
+  "https://cdn.discordapp.com/attachments/828079037565108265/953532727246868550/download.gif"
 ]
 
 makers_mock_data = [
